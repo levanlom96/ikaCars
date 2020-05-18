23a2f3c9a7b038529f793c4b12ef5b0622300183
IrakliMeto <ika.metonidze@gmail.com>
2020-05-18 10:50:25 +0400

    change something

--- lesson2.js
+++ lesson2.js
@@ -9,118 +9,126 @@
 /////////////////////////////////////////////
 //chalenge 3
 ///////////////////////////////////////////////
-let car = {
-  marka: "Toyota",
-  model: "Highlender",
-  color: "Grey",
-  year: 2018,
-  horseForce: 370,
-  price: 37777,
-};
-
-console.log("About this car : ");
-
-for (let key in car) {
-  console.log(`${key} , ${car[key]}`);
-}
-console.log("\n");
-console.log("\n");
-////////////////////////////////////////////////////
-/////////////
-
-let car1 = {
-  marka: "Toyota",
-  model: "Prius",
-  color: "Grey",
-  year: 2018,
-  horseForce: 160,
-  price: 13333,
-};
-
-console.log("About this car : ");
-for (let key in car1) {
-  console.log(`${key}, ${car1[key]}`);
-}
-
-console.log("\n");
-console.log("\n");
-////////////////////////////////////
-////
-let car2 = {
-  marka: "BMW",
-  model: "E35",
-  color: "Grey",
-  year: 2012,
-  horseForce: 280,
-  price: 11111,
-};
-
-console.log("About this car : ");
-for (let key in car2) {
-  console.log(`${key}, ${car2[key]}`);
-}
-
-console.log("\n");
-console.log("\n");
-/////////////////////////////////////////////////
-/////
-let car3 = {
-  marka: "Mercedes",
-  model: "GL",
-  color: "Grey",
-  year: 2016,
-  horseForce: 450,
-  price: 28000,
-};
-
-console.log("About this car : ");
-for (let key in car3) {
-  console.log(`${key}, ${car3[key]}`);
-}
-console.log("\n");
-console.log("\n");
 // let car = {
 //   marka: "Toyota",
 //   model: "Highlender",
 //   color: "Grey",
 //   year: 2018,
-//   horseForce: 370,
+//   horsePower: 370,
 //   price: 37777,
 // };
+
+// console.log("About this car : ");
+
+// for (let key in car) {
+//   console.log(`${key} , ${car[key]}`);
+// }
+// console.log("\n");
+// console.log("\n");
+// ////////////////////////////////////////////////////
+// /////////////
 
 // let car1 = {
 //   marka: "Toyota",
 //   model: "Prius",
 //   color: "Grey",
 //   year: 2018,
-//   horseForce: 160,
+//   horsePower: 160,
 //   price: 13333,
 // };
 
+// console.log("About this car : ");
+// for (let key in car1) {
+//   console.log(`${key}, ${car1[key]}`);
+// }
+
+// console.log("\n");
+// console.log("\n");
+// ////////////////////////////////////
+// ////
 // let car2 = {
 //   marka: "BMW",
 //   model: "E35",
 //   color: "Grey",
 //   year: 2012,
-//   horseForce: 280,
+//   horsePower: 280,
 //   price: 11111,
 // };
 
+// console.log("About this car : ");
+// for (let key in car2) {
+//   console.log(`${key}, ${car2[key]}`);
+// }
+
+// console.log("\n");
+// console.log("\n");
+// ///////////////////////////////////////////////
+// ///
 // let car3 = {
 //   marka: "Mercedes",
 //   model: "GL",
 //   color: "Grey",
 //   year: 2016,
-//   horseForce: 450,
+//   horsePower: 450,
 //   price: 28000,
 // };
 
-// let allCars = [car, car1, car2, car3];
+// console.log(
+//   `About car: \n Marka: ${car3.marka}, \n Color: ${car3.color}, \n Model: ${car3.model}, \n Year: ${car3.year}, \n HorsePower: ${car3.horsePower}, \n Price: ${car3.price}`
+// );
 
-// for (i = 0; i < allCars.length; i++) {
-//   if (allCars[i].price < "15000$") {
-//     console.log(`yes we have some car's :  ${allCars[i].model}`);
-//   } else {
-//     console.log("Sorry bu, we don't have a similar price car's ");
-//   }
-// }
+// console.log("\n");
+// console.log("\n");
+
+/////////////////////////////
+//сhalenge 4
+//////////////////////////
+
+let car = {
+  marka: "Toyota",
+  model: "Highlender",
+  color: "Grey",
+  year: 2018,
+  horsePower: 370,
+  price: 31777,
+};
+
+let car1 = {
+  marka: "Toyota",
+  model: "Prius",
+  color: "Grey",
+  year: 2018,
+  horsePower: 160,
+  price: 13000,
+};
+
+let car2 = {
+  marka: "BMW",
+  model: "E35",
+  color: "Grey",
+  year: 2012,
+  horsePower: 280,
+  price: 8900,
+};
+
+let car3 = {
+  marka: "Mercedes",
+  model: "GL",
+  color: "Grey",
+  year: 2016,
+  horsePower: 450,
+  price: 28000,
+};
+
+let allCars = [car, car1, car2, car3];
+let carFound = false;
+for (i = 0; i < allCars.length; i++) {
+  if (allCars[i].price < 15000) {
+    carFound = true;
+  }
+}
+if (carFound) {
+  console.log("Yes, we have some car's.");
+} else {
+  console.log("No, sorry we dont have car's in this price.");
+}
